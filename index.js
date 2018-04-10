15ebab21c8c71fce82ad90f956d433b649cd6747
Glen Chiacchieri <www@glench.com>
2018-04-10 01:05:23 -0400

    working

--- index.js
+++ index.js
@@ -2,9 +2,34 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
+const ws = require('ws')
+const bodyParser = require('body-parser');
+
+var recorded_time = "14:00";
+
+const app = express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(bodyParser.urlencoded({ extended: true }))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .post('/set-time', function(req, res) {
+       recorded_time = req.body.time;
+       res.send('ok')
+
+       // when someone sets the time, update everyone else's client
+       wss.clients.forEach(function (client) {
+          client.send(recorded_time);
+       });
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+const wss = new ws.Server({port: 40510});
+
+wss.on('connection', function(ws, req) {
+    // on connect, send the meeting time
+    ws.send(recorded_time)
+})
+
+
